db1fa58238ddfaebb6d1fe51220c2e791af0c50c
Amjad Shadid <amjadshadid51@gmail.com>
2020-02-04 11:18:06 +0200

    Initial Commit

---
+++ tutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorials
+//
+//  Created by Amjad Shadid on 2/4/20.
+//  Copyright © 2020 Amjad Shadid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
