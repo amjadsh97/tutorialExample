671f2fbaede9ec046cacb0aa4433ae007e4e221a
Amjad Shadid <amjadshadid51@gmail.com>
2020-02-07 19:46:35 +0200

    =add c++ file

---
+++ tutorials/amjads/test.cpp
@@ -0,0 +1,11 @@
+//
+//  test.cpp
+//  tutorials
+//
+//  Created by Amjad Shadid on 2/7/20.
+//  Copyright © 2020 Amjad Shadid. All rights reserved.
+//
+
+#include "test.hpp"
+//something wrong here 
+//spmething wrong here
---
+++ tutorials/amjads/test.hpp
@@ -0,0 +1,16 @@
+//
+//  test.hpp
+//  tutorials
+//
+//  Created by Amjad Shadid on 2/7/20.
+//  Copyright © 2020 Amjad Shadid. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
+//spmething wrong here
+//spmething wrong here
